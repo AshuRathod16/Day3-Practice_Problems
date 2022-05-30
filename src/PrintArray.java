0e8af5a5aceaaf183ca70d4c3f52e971af7919ae
Ashwini Rathod <rashwini510@gmail.com>
2022-05-30 16:00:52 +0530

    Addede: Print the elements of array

---
+++ src/PrintArray.java
@@ -0,0 +1,11 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        System.out.println("Elements of given array:");
+        int i;
+        for (i = 0; i < array.length; i++) {
+            System.out.println(array[i] + "");
+        }
+    }
+
+}
